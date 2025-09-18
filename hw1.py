52a847b7e17b5394ccbdaea122e30fb1a7ee3587
lps15 <lpschneier15@gmail.com>
2025-09-18 18:35:25 -0400

    first changes

--- hw1.py
+++ hw1.py
@@ -1,8 +1,17 @@
 import cv2
+import os
 import numpy as np
-from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
+IMG = 'images'
+SIZE = (100,100)
+out_dir = 'images_with_filters'
 
+for filename in os.listdir(IMG):
+    path = os.path.join(IMG, filename)
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
 
+    img_resized = cv2.resize(img, SIZE)
+    out_path = os.path.join(out_dir, filename)
+    cv2.imwrite(out_path, img_resized)
 
